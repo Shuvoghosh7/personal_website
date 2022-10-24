c5297054028bff7dab61ea165e7dff6e2ce6cf73
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-10-24 12:28:27 +0600

    complete banner part

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { Button, Col, Container, Placeholder, Row } from 'react-bootstrap';
+import { AiTwotoneStar } from 'react-icons/ai';
+import { BsStarHalf } from 'react-icons/bs';
+import { GiSelfLove } from 'react-icons/gi';
+import banner from '../../images/banner.png'
+import banner2 from '../../images/semrush.png'
 
-
+import './Banner.css'
 const Banner = () => {
     return (
-        <div>
-            
-            
+        <div className='banner-part'>
+            <div>
+                <img className='banner' src={banner} alt="" />
+                <p className='bg-dark'>p</p>
+            </div>
+            <div className='d-lg-flex justify-content-between'>
+                <div>
+                    <div className='d-flex justify-content-around gap-5'>
+                        <div className='title-image'>
+                            <img src={banner2} alt="" />
+                        </div>
+                        <div>
+                            <h1>Semrush</h1>
+                            <div className='d-lg-flex d-md-flex justify-content-center align-items-center gap-3'>
+                                <div className='d-flex react-icon'>
+                                    <h3><AiTwotoneStar /></h3>
+                                    <h3><AiTwotoneStar /></h3>
+                                    <h3><AiTwotoneStar /></h3>
+                                    <h3><BsStarHalf /></h3>
+                                </div>
+                                <div className='mt-3'>
+                                    <p className='reviews'><span >1,635 reviews</span> | <span>29 discussions</span></p>
+                                </div>
+                            </div>
+                            <div>
+                                <p><GiSelfLove /> <span>Favorite | <span>Claimed</span></span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <Button variant="outline-warning" className='me-3'>Contact Semrush</Button>
+                    <Button variant="success" className='me-3'>Try it for free</Button>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 
 const Home = () => {
     return (
-        <div>
-            heloo
+        <div className='mx-5'>
+            <Banner/>
+             
         </div>
     );
 };
